088139f6533c046c165b45d856043b0255d19015
ayushnaithani21 <104084677+ayushnaithani21@users.noreply.github.com>
2022-06-22 15:27:04 +0530

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,5 +1,14 @@
-print(range(5))
-print(range(3, 10))
-print(range(4, 13, 3))
-print(range(15, 5, -2))
-print(range(5, 3))
+for i in range(5):
+  print(i)
+  
+for i in range(3,10):
+  print(i)
+  
+for i in range(4,13,3):
+  print(i)
+  
+for i in range(15,5,-2):
+  print(i)
+  
+for i in range(5,3):
+  print(i)
